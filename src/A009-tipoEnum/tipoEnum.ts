2fcb86e409ab6fca2e7635c81e137bdc0a48cbf7
jair-correa <jair_correa@outlook.com.br>
2025-03-03 16:10:24 -0300

    Tipo Enum

---
+++ src/A009-tipoEnum/tipoEnum.ts
@@ -0,0 +1,17 @@
+enum Cores {
+  VERDE = "green",
+  AZUL = "blue",
+  VERMELHO = "red",
+  AMARELO = "yellow",
+  ROSA = "pink",
+}
+enum Cores {
+  ROXO = "purple",
+  LIALS = "orange",
+  CIANO = "lightpink",
+}
+
+function escolhaCor(cor: Cores): void {
+  console.log(Cores); //;
+}
+
