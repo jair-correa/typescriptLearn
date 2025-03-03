bbf98d2ec21cfcca87c0143b3b0b09797c0a4a09
jair-correa <jair_correa@outlook.com.br>
2025-03-03 13:52:54 -0300

    Type Any

---
+++ src/A002-typAny/typAny.ts
@@ -0,0 +1,12 @@
+function showMessage(msg:any) {
+  return msg;
+}
+/*
+function showMessage(msg:any) {
+  return msg;
+}
+ */
+
+console.log(showMessage([1,2,3]));
+console.log(showMessage('Hello'));
+console.log(showMessage(1));
