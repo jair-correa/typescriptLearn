4de638275453d8f2be9bad007b1a47d7f4a97783
jair-correa <jair_correa@outlook.com.br>
2025-03-04 12:33:29 -0300

    Update frontend code and add favicon_io

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const { config } = require('process');
 
 module.exports = {
   mode: 'development',
-  entry: './src/A018-webpack/index.ts',
+  entry: './src/A019-Exercise/A019-Exercise.ts',
   module: {
     rules: [
       {
@@ -24,4 +23,14 @@ module.exports = {
     path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
   },
   devtool: 'source-map',
+  
+  // Configuração do devServer
+  devServer: {
+    contentBase: path.join(__dirname, 'frontend'),  // Diretorio onde os arquivos estáticos serão servidos
+    compress: true,
+    port: 5500, // Porta onde o servidor será iniciado
+    historyApiFallback: true, // Se você estiver usando React Router ou SPA, isso resolve o erro 404 nas rotas
+    publicPath: '/assets/js/', // Diretório onde os arquivos de bundle.js serão acessados
+    open: true, // Abre automaticamente o navegador quando o servidor for iniciado
+  },
 };
